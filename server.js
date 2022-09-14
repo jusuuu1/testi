60b4fd12249f6e0231bb04474831ccfc2a4622a8
jusuuu1 <juuso.porhala2@gmail.com>
2022-09-14 09:24:09 +0200

    serverin perusteiden luonti

--- server.js
+++ server.js
@@ -10,3 +10,12 @@ const mongodb = reguire ("mongodb");
 
 // Ota bodyparser käyttöön lomakkeen käsittelyä varten
 const bodyparser = reguire ("body-parser");
+
+// Aseta määritykset express palvelimelle
+// Ota käyttöön public-tiedosto
+app.use(express.static("public"));
+//Ota käyttöön bodyparser
+app.use(bodyparser.urlencoded({extended:false}));
+
+// Laitetaan palvelin kuuntelemaan porttia 8080
+const server = app.listen(8080, function (){})
