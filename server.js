d588aebe3723182152bf6b1f7dddcac33ff7f12b
jusuuu1 <juuso.porhala2@gmail.com>
2022-09-14 09:17:37 +0200

    init

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ const app = express ();
 // Ota mongoose käyttöön -> tietokantayhteys
 const mongoose = reguire("mongoose");
 
+// Ota mongodb käyttöön
+const mongodb = reguire ("mongodb");
+
 // Ota bodyparser käyttöön lomakkeen käsittelyä varten
 const bodyparser = reguire ("body-parser");
